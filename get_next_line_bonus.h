f9c87fed4fe695b345406a843b5fb93ed6593721
OrioPrisco <47635210+OrioPrisco@users.noreply.github.com>
2022-11-15 18:02:13 +0100

    bonus part, same as main one

---
+++ get_next_line_bonus.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: OrioPrisco <47635210+OrioPrisco@users      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/12 17:03:01 by OrioPrisc         #+#    #+#             */
+/*   Updated: 2022/11/12 18:40:02 by OrioPrisc        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stddef.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 100
+# endif
+
+char	*get_next_line(int fd);
+char	*ft_strchrnul(const char *s, char c);
+size_t	ft_strlen(const char *c);
+int		ft_strjoin_overflow(char **previous, char *buffer);
+size_t	ft_strlcpy(char *dest, const char *src, size_t size);
+
+#endif
---
+++ get_next_line_utils_bonus.c
@@ -0,0 +1,81 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: OrioPrisco <47635210+OrioPrisco@users      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/12 12:15:56 by OrioPrisc         #+#    #+#             */
+/*   Updated: 2022/11/12 18:51:05 by OrioPrisc        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+#include <stdlib.h>
+
+char	*ft_strchrnul(const char *s, char c)
+{
+	if (c == '\0')
+		return ((char *)s + ft_strlen(s));
+	while (*s)
+	{
+		if (*s == c)
+			return ((char *)s);
+		s++;
+	}
+	return ((char *)s);
+}
+
+size_t	ft_strlen(const char *c)
+{
+	size_t	len;
+
+	len = 0;
+	while (*c++)
+		len++;
+	return (len);
+}
+
+//joins *previous and buffer into previous up until a \n.
+//Things after the \n gets copied into buffer
+//returns wether a \n was seen
+int	ft_strjoin_overflow(char **previous, char *buffer)
+{
+	char	*str;
+	char	*next;
+	size_t	lprev;
+	size_t	lbuff;
+
+	next = ft_strchrnul(buffer, '\n');
+	lbuff = next - buffer + (*next == '\n');
+	lprev = 0;
+	if (*previous)
+		lprev = ft_strlen(*previous);
+	str = malloc(lprev + lbuff + 1);
+	if (!str)
+		return (0);
+	if (*previous)
+		ft_strlcpy(str, *previous, lprev + 1);
+	ft_strlcpy(str + lprev, buffer, lbuff + 1);
+	ft_strlcpy(buffer, next + (*next == '\n'), BUFFER_SIZE + 1 - lbuff);
+	free(*previous);
+	*previous = str;
+	return (*next == '\n');
+}
+
+size_t	ft_strlcpy(char *dest, const char *src, size_t size)
+{
+	size_t	i;
+
+	i = 0;
+	if (size != 0)
+	{
+		while ((i < size - 1) && src[i])
+		{
+			dest[i] = src[i];
+			i++;
+		}
+		dest[i] = '\0';
+	}
+	return (ft_strlen(src));
+}
