fa1dcd955b3954fac3b374e580827f96a75c8206
OrioPrisco <47635210+OrioPrisco@users.noreply.github.com>
2022-11-18 09:59:56 +0100

    correct include for bonus utils

--- get_next_line_utils_bonus.c
+++ get_next_line_utils_bonus.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "get_next_line_bonus.h"
 #include <stdlib.h>
 
 char	*ft_strchrnul(const char *s, char c)
